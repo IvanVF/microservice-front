d25e19ddb9e4e101fbfb8f0b3a39d14d8cbe5cbe
IvanVF <fedinivanvladimirovich@mail.ru>
2021-12-23 23:07:48 +0700

    GET and POST works

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,9 +16,22 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.getUsers().subscribe(value => {});
     console.log("users: ", this.getUsers().subscribe(value => {}));
+    //this.getImage().subscribe((data:any) => this.imageEntity = new ImageEntity(data.id, data.imageBlob, data.description, data.type));
+    let a=10;
+    let b=11;
+    this.postImage().subscribe(value => {});
   }
 
   getUsers(): Observable<any> {
-    return this.http.get("http://localhost:8200/users")
+    return this.http.get("http://localhost:8200/users");
+  }
+
+  getImage(): Observable<any> {
+    return this.http.get("http://localhost:8200/image?id=2");
+  }
+
+  postImage(): Observable<any> {
+    const body = {imgAddr: 123, description: "descr", type: "dont know"};
+    return this.http.post("http://localhost:8200/image", body);
   }
 }
