1cd37bc3bcb9a1a7c256283bd9a7b3b4328985e3
IvanVF <fedinivanvladimirovich@mail.ru>
2022-01-11 15:29:32 +0700

    Body-home layout

--- src/app/components/body-home/body-home.component.ts
+++ src/app/components/body-home/body-home.component.ts
@@ -8,7 +8,14 @@ import {BicyclesService} from "../../services/bicycles/bicycles.service";
 })
 export class BodyHomeComponent implements OnInit {
   items: any;
-  requestParams= new Map();
+  requestParams = new Map();
+  selectedItem = new Map<string, boolean> ([
+    ["BICYCLES", false],
+    ["SCOOTERS", false],
+    ["ACCESSORIES", false],
+    ["EQUIPMENT", false],
+    ["SPARES", false],
+  ]);
 
   constructor(
     private bicycleService: BicyclesService,
@@ -21,9 +28,58 @@ export class BodyHomeComponent implements OnInit {
       .pipe()
       .subscribe(res => {
         this.items = res;
-        let abc: string = "asd";
-        console.log(abc);
-        console.log(this.items);
+        this.selectedItem.set("BICYCLES", true)
       });
   }
+
+  /**
+   * Set value true for selected item (bicycle, scooter e.t.c) in Map selectedItem
+   * @param itemType
+   */
+  refreshItemType(itemType: string) {
+    for (let key of this.selectedItem.keys()) {
+      this.selectedItem.set(key, false);
+    }
+    this.selectedItem.set(itemType, true);
+  }
+
+  /**
+   * Load information for selected item
+   * and set item selected
+   * @param itemType
+   */
+  changeItems(itemType: string) {
+    this.refreshItemType(itemType);
+    switch (itemType) {
+      case "BICYCLES": {
+        this.bicycleService.getBicycles(this.requestParams)
+          .pipe()
+          .subscribe(res => {
+            this.items = res;
+          });
+        break;
+      }
+      case "SCOOTERS": {
+        console.log("switched to scooters");
+        break;
+      }
+      case "ACCESSORIES": {
+        console.log("switched to accessories");
+        break;
+      }
+      case "EQUIPMENT": {
+        console.log("switched to equipment");
+        break;
+      }
+      case "SPARES": {
+        console.log("switched to spares");
+        break;
+      }
+      default: {
+        console.log("Invalid choice");
+        break;
+      }
+    }
+  }
+
 }
