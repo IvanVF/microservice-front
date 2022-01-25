6575c8c201269dc89d0d74c1c72c825541884c63
IvanVF <fedinivanvladimirovich@mail.ru>
2022-01-25 11:06:18 +0700

    Change microserviceBackCoreURL to heroku URL.

--- src/app/services/urls.service.ts
+++ src/app/services/urls.service.ts
@@ -4,7 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class URLsService {
-  public microserviceBackCoreURL: string = "http://localhost:8341/";
+  // Local microservice-back-core URL:
+  // public microserviceBackCoreURL: string = "http://localhost:8341/";
+
+  // Heroku microservice-back-core URL:
+  public microserviceBackCoreURL: string = "https://bicycle-shop-back-core.herokuapp.com/";
 
   constructor() { }
 }
